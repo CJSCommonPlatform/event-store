CJSCommonPlatform/microservice_framework_version_6

Refactor Envelope payload to support JsonValue

event-sourcing/event-publisher/event-publisher-core/src/main/java/uk/gov/justice/services/eventsourcing/publisher/core/EventPublisher.java
@@ -1,7 +1,7 @@
 package uk.gov.justice.services.eventsourcing.publisher.core;
 
 
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 /**
  * Interface for a service that can publish events.
@@ -12,7 +12,7 @@ public interface EventPublisher {
     /**
      * Publish event that has been raised on the stream.
      *
-     * @param envelope containing metadata and event.
+     * @param jsonEnvelope containing metadata and event.
      */
-    void publish(final Envelope envelope);
+    void publish(final JsonEnvelope jsonEnvelope);
 }

event-sourcing/event-publisher/event-publisher-jms/src/main/java/uk/gov/justice/services/eventsourcing/publisher/jms/JmsEventPublisher.java
@@ -1,7 +1,7 @@
 package uk.gov.justice.services.eventsourcing.publisher.jms;
 
 import uk.gov.justice.services.eventsourcing.publisher.core.EventPublisher;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 import uk.gov.justice.services.messaging.jms.JmsEnvelopeSender;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -20,8 +20,8 @@ public class JmsEventPublisher implements EventPublisher {
     MessagingDestinationResolver messagingDestinationResolver;
 
     @Override
-    public void publish(final Envelope envelope) {
-        jmsEnvelopeSender.send(envelope, messagingDestinationResolver.resolve(envelope.metadata().name()));
+    public void publish(final JsonEnvelope jsonEnvelope) {
+        jmsEnvelopeSender.send(jsonEnvelope, messagingDestinationResolver.resolve(jsonEnvelope.metadata().name()));
     }
 
 }

event-sourcing/event-publisher/event-publisher-jms/src/test/java/uk/gov/justice/services/eventsourcing/publisher/jms/JmsEventPublisherTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 import uk.gov.justice.services.messaging.Metadata;
 import uk.gov.justice.services.messaging.jms.JmsEnvelopeSender;
 
@@ -28,7 +28,7 @@ public class JmsEventPublisherTest {
     private Destination destination;
 
     @Mock
-    private Envelope envelope;
+    private JsonEnvelope jsonEnvelope;
 
     @Mock
     private Metadata metadata;
@@ -38,13 +38,13 @@ public class JmsEventPublisherTest {
         JmsEventPublisher jmsEventPublisher = new JmsEventPublisher();
         jmsEventPublisher.jmsEnvelopeSender = jmsEnvelopeSender;
         jmsEventPublisher.messagingDestinationResolver = messagingDestinationResolver;
-        when(envelope.metadata()).thenReturn(metadata);
+        when(jsonEnvelope.metadata()).thenReturn(metadata);
         when(metadata.name()).thenReturn(NAME);
         when(messagingDestinationResolver.resolve(NAME)).thenReturn(destination);
 
-        jmsEventPublisher.publish(envelope);
+        jmsEventPublisher.publish(jsonEnvelope);
 
-        verify(jmsEnvelopeSender).send(envelope, destination);
+        verify(jmsEnvelopeSender).send(jsonEnvelope, destination);
     }
 
 }

event-sourcing/event-repository/event-repository-jdbc/src/main/java/uk/gov/justice/services/eventsourcing/repository/jdbc/JdbcEventRepository.java
@@ -8,7 +8,7 @@ import uk.gov.justice.services.eventsourcing.repository.jdbc.eventlog.JdbcEventL
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.EventLogRepositoryException;
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.InvalidSequenceIdException;
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.InvalidStreamIdException;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -27,7 +27,7 @@ public class JdbcEventRepository implements EventRepository {
     JdbcEventLogRepository jdbcEventLogRepository;
 
     @Override
-    public Stream<Envelope> getByStreamId(final UUID streamId) {
+    public Stream<JsonEnvelope> getByStreamId(final UUID streamId) {
         if (streamId == null) {
             throw new InvalidStreamIdException("streamId is null.");
         }
@@ -37,7 +37,7 @@ public class JdbcEventRepository implements EventRepository {
     }
 
     @Override
-    public Stream<Envelope> getByStreamIdAndSequenceId(final UUID streamId, final Long sequenceId) {
+    public Stream<JsonEnvelope> getByStreamIdAndSequenceId(final UUID streamId, final Long sequenceId) {
         if (streamId == null) {
             throw new InvalidStreamIdException("streamId is null.");
         } else if (sequenceId == null) {
@@ -51,13 +51,13 @@ public class JdbcEventRepository implements EventRepository {
 
     @Override
     @Transactional
-    public void store(final Envelope envelope, final UUID streamId, final Long version) throws StoreEventRequestFailedException {
+    public void store(final JsonEnvelope jsonEnvelope, final UUID streamId, final Long version) throws StoreEventRequestFailedException {
         try {
-            final EventLog eventLog = eventLogConverter.createEventLog(envelope, streamId, version);
+            final EventLog eventLog = eventLogConverter.createEventLog(jsonEnvelope, streamId, version);
             jdbcEventLogRepository.insert(eventLog);
         } catch (InvalidSequenceIdException ex) {
             throw new StoreEventRequestFailedException(String.format("Could not store event for version %d of stream %s",
-                    envelope.metadata().version().orElse(null), envelope.metadata().streamId().orElse(null)), ex);
+                    jsonEnvelope.metadata().version().orElse(null), jsonEnvelope.metadata().streamId().orElse(null)), ex);
         }
     }
 

event-sourcing/event-repository/event-repository-jdbc/src/main/java/uk/gov/justice/services/eventsourcing/repository/jdbc/eventlog/EventLogConverter.java
@@ -3,8 +3,8 @@ package uk.gov.justice.services.eventsourcing.repository.jdbc.eventlog;
 import uk.gov.justice.services.common.converter.JsonObjectToStringConverter;
 import uk.gov.justice.services.common.converter.StringToJsonObjectConverter;
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.InvalidStreamIdException;
-import uk.gov.justice.services.messaging.DefaultEnvelope;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.DefaultJsonEnvelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 import uk.gov.justice.services.messaging.JsonObjectEnvelopeConverter;
 import uk.gov.justice.services.messaging.JsonObjectMetadata;
 import uk.gov.justice.services.messaging.Metadata;
@@ -15,7 +15,7 @@ import javax.json.JsonObject;
 import java.util.UUID;
 
 /**
- * Converter class to convert between {@link Envelope} and {@link EventLog}
+ * Converter class to convert between {@link JsonEnvelope} and {@link EventLog}
  */
 @ApplicationScoped
 public class EventLogConverter {
@@ -32,36 +32,36 @@ public class EventLogConverter {
     /**
      * Creates an {@link EventLog} object from the <code>eventEnvelope</code>.
      *
-     * @param envelope the envelope to convert from
+     * @param jsonEnvelope the jsonEnvelope to convert from
      * @param streamId the stream id the event belongs to
      * @param version  the version of the event
-     * @return the database entity created from the given envelope
+     * @return the database entity created from the given jsonEnvelope
      */
-    public EventLog createEventLog(final Envelope envelope, final UUID streamId, final Long version) {
+    public EventLog createEventLog(final JsonEnvelope jsonEnvelope, final UUID streamId, final Long version) {
 
         if (streamId == null) {
-            throw new InvalidStreamIdException("StreamId missing in envelope.");
+            throw new InvalidStreamIdException("StreamId missing in jsonEnvelope.");
         }
 
-        final Metadata eventMetadata = envelope.metadata();
+        final Metadata eventMetadata = jsonEnvelope.metadata();
 
         return new EventLog(eventMetadata.id(),
                 streamId,
                 version,
                 eventMetadata.name(),
-                jsonObjectToStringConverter.convert(envelope.metadata().asJsonObject()),
-                extractPayloadAsString(envelope));
+                jsonObjectToStringConverter.convert(jsonEnvelope.metadata().asJsonObject()),
+                extractPayloadAsString(jsonEnvelope));
 
     }
 
     /**
-     * Creates an {@link Envelope} from {@link EventLog}
+     * Creates an {@link JsonEnvelope} from {@link EventLog}
      *
      * @param eventLog eventLog to be converted into an envelope.
      * @return an envelope created from eventLog.
      */
-    public Envelope createEnvelope(final EventLog eventLog) {
-        return DefaultEnvelope.envelopeFrom(getMetaData(eventLog), getPayload(eventLog));
+    public JsonEnvelope createEnvelope(final EventLog eventLog) {
+        return DefaultJsonEnvelope.envelopeFrom(getMetaData(eventLog), getPayload(eventLog));
     }
 
     /**
@@ -78,9 +78,9 @@ public class EventLogConverter {
         return stringToJsonObjectConverter.convert(eventLog.getPayload());
     }
 
-    private String extractPayloadAsString(final Envelope envelope) {
+    private String extractPayloadAsString(final JsonEnvelope jsonEnvelope) {
         return jsonObjectToStringConverter.convert(jsonObjectEnvelopeConverter.extractPayloadFromEnvelope(
-                jsonObjectEnvelopeConverter.fromEnvelope(envelope)));
+                jsonObjectEnvelopeConverter.fromEnvelope(jsonEnvelope)));
     }
 
 }

event-sourcing/event-repository/event-repository-jdbc/src/test/java/uk/gov/justice/services/eventsourcing/repository/jdbc/JdbcEventRepositoryTest.java
@@ -12,7 +12,7 @@ import uk.gov.justice.services.eventsourcing.repository.jdbc.eventlog.JdbcEventL
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.EventLogRepositoryException;
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.InvalidSequenceIdException;
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.InvalidStreamIdException;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 import uk.gov.justice.services.messaging.Metadata;
 
 import java.util.Arrays;
@@ -44,7 +44,7 @@ public class JdbcEventRepositoryTest {
     private EventLogConverter eventLogConverter;
 
     @Mock
-    private Envelope envelope;
+    private JsonEnvelope jsonEnvelope;
 
     @Mock
     private Metadata metadata;
@@ -58,23 +58,23 @@ public class JdbcEventRepositoryTest {
     @Test
     public void shouldGetByStreamId() throws Exception {
         when(jdbcEventLogRepository.findByStreamIdOrderBySequenceIdAsc(STREAM_ID)).thenReturn(Arrays.asList(eventLog).stream());
-        when(eventLogConverter.createEnvelope(eventLog)).thenReturn(envelope);
+        when(eventLogConverter.createEnvelope(eventLog)).thenReturn(jsonEnvelope);
 
-        Stream<Envelope> streamOfEnvelopes = jdbcEventRepository.getByStreamId(STREAM_ID);
+        Stream<JsonEnvelope> streamOfEnvelopes = jdbcEventRepository.getByStreamId(STREAM_ID);
 
         assertThat(streamOfEnvelopes, not(nullValue()));
-        assertThat(streamOfEnvelopes.findFirst().get(), equalTo(envelope));
+        assertThat(streamOfEnvelopes.findFirst().get(), equalTo(jsonEnvelope));
     }
 
     @Test
     public void shouldGetByStreamIdAndSequenceId() throws Exception {
         when(jdbcEventLogRepository.findByStreamIdFromSequenceIdOrderBySequenceIdAsc(STREAM_ID, VERSION_1)).thenReturn(Arrays.asList(eventLog).stream());
-        when(eventLogConverter.createEnvelope(eventLog)).thenReturn(envelope);
+        when(eventLogConverter.createEnvelope(eventLog)).thenReturn(jsonEnvelope);
 
-        Stream<Envelope> streamOfEnvelopes = jdbcEventRepository.getByStreamIdAndSequenceId(STREAM_ID, VERSION_1);
+        Stream<JsonEnvelope> streamOfEnvelopes = jdbcEventRepository.getByStreamIdAndSequenceId(STREAM_ID, VERSION_1);
 
         assertThat(streamOfEnvelopes, not(nullValue()));
-        assertThat(streamOfEnvelopes.findFirst().get(), equalTo(envelope));
+        assertThat(streamOfEnvelopes.findFirst().get(), equalTo(jsonEnvelope));
     }
 
     @Test(expected = InvalidStreamIdException.class)
@@ -94,23 +94,23 @@ public class JdbcEventRepositoryTest {
 
     @Test
     public void shouldStoreEnvelope() throws Exception {
-        when(eventLogConverter.createEventLog(envelope, STREAM_ID, VERSION_1)).thenReturn(eventLog);
+        when(eventLogConverter.createEventLog(jsonEnvelope, STREAM_ID, VERSION_1)).thenReturn(eventLog);
 
-        jdbcEventRepository.store(envelope, STREAM_ID, VERSION_1);
+        jdbcEventRepository.store(jsonEnvelope, STREAM_ID, VERSION_1);
 
         verify(jdbcEventLogRepository).insert(eventLog);
     }
 
     @Test(expected = StoreEventRequestFailedException.class)
     public void shouldThrowExceptionOnDuplicateVersion() throws Exception {
-        when(eventLogConverter.createEventLog(envelope, STREAM_ID, VERSION_1)).thenReturn(eventLog);
-        when(envelope.metadata()).thenReturn(metadata);
+        when(eventLogConverter.createEventLog(jsonEnvelope, STREAM_ID, VERSION_1)).thenReturn(eventLog);
+        when(jsonEnvelope.metadata()).thenReturn(metadata);
         when(metadata.streamId()).thenReturn(Optional.of(STREAM_ID));
         when(metadata.version()).thenReturn(Optional.of(VERSION_1));
 
         doThrow(InvalidSequenceIdException.class).when(jdbcEventLogRepository).insert(eventLog);
 
-        jdbcEventRepository.store(envelope, STREAM_ID, VERSION_1);
+        jdbcEventRepository.store(jsonEnvelope, STREAM_ID, VERSION_1);
     }
 
     @Test

CJSCommonPlatform/event-store

event-sourcing/event-publisher/event-publisher-core/src/main/java/uk/gov/justice/services/eventsourcing/publisher/core/EventPublisher.java
@@ -1,7 +1,7 @@
 package uk.gov.justice.services.eventsourcing.publisher.core;
 
 
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 /**
  * Interface for a service that can publish events.
@@ -14,5 +14,5 @@ public interface EventPublisher {
      *
      * @param envelope containing metadata and event.
      */
-    void publish(final Envelope envelope);
+    void publish(final JsonEnvelope envelope);
 }

event-sourcing/event-publisher/event-publisher-jms/src/main/java/uk/gov/justice/services/eventsourcing/publisher/jms/JmsEventPublisher.java
@@ -1,7 +1,7 @@
 package uk.gov.justice.services.eventsourcing.publisher.jms;
 
 import uk.gov.justice.services.eventsourcing.publisher.core.EventPublisher;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 import uk.gov.justice.services.messaging.jms.JmsEnvelopeSender;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -20,7 +20,7 @@ public class JmsEventPublisher implements EventPublisher {
     MessagingDestinationResolver messagingDestinationResolver;
 
     @Override
-    public void publish(final Envelope envelope) {
+    public void publish(final JsonEnvelope envelope) {
         jmsEnvelopeSender.send(envelope, messagingDestinationResolver.resolve(envelope.metadata().name()));
     }
 

event-sourcing/event-publisher/event-publisher-jms/src/test/java/uk/gov/justice/services/eventsourcing/publisher/jms/JmsEventPublisherTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 import uk.gov.justice.services.messaging.Metadata;
 import uk.gov.justice.services.messaging.jms.JmsEnvelopeSender;
 
@@ -28,7 +28,7 @@ public class JmsEventPublisherTest {
     private Destination destination;
 
     @Mock
-    private Envelope envelope;
+    private JsonEnvelope envelope;
 
     @Mock
     private Metadata metadata;

event-sourcing/event-repository/event-repository-jdbc/src/main/java/uk/gov/justice/services/eventsourcing/repository/jdbc/JdbcEventRepository.java
@@ -8,7 +8,7 @@ import uk.gov.justice.services.eventsourcing.repository.jdbc.eventlog.JdbcEventL
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.EventLogRepositoryException;
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.InvalidSequenceIdException;
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.InvalidStreamIdException;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -27,7 +27,7 @@ public class JdbcEventRepository implements EventRepository {
     JdbcEventLogRepository jdbcEventLogRepository;
 
     @Override
-    public Stream<Envelope> getByStreamId(final UUID streamId) {
+    public Stream<JsonEnvelope> getByStreamId(final UUID streamId) {
         if (streamId == null) {
             throw new InvalidStreamIdException("streamId is null.");
         }
@@ -37,7 +37,7 @@ public class JdbcEventRepository implements EventRepository {
     }
 
     @Override
-    public Stream<Envelope> getByStreamIdAndSequenceId(final UUID streamId, final Long sequenceId) {
+    public Stream<JsonEnvelope> getByStreamIdAndSequenceId(final UUID streamId, final Long sequenceId) {
         if (streamId == null) {
             throw new InvalidStreamIdException("streamId is null.");
         } else if (sequenceId == null) {
@@ -51,7 +51,7 @@ public class JdbcEventRepository implements EventRepository {
 
     @Override
     @Transactional
-    public void store(final Envelope envelope, final UUID streamId, final Long version) throws StoreEventRequestFailedException {
+    public void store(final JsonEnvelope envelope, final UUID streamId, final Long version) throws StoreEventRequestFailedException {
         try {
             final EventLog eventLog = eventLogConverter.createEventLog(envelope, streamId, version);
             jdbcEventLogRepository.insert(eventLog);

event-sourcing/event-repository/event-repository-jdbc/src/main/java/uk/gov/justice/services/eventsourcing/repository/jdbc/eventlog/EventLogConverter.java
@@ -3,8 +3,8 @@ package uk.gov.justice.services.eventsourcing.repository.jdbc.eventlog;
 import uk.gov.justice.services.common.converter.JsonObjectToStringConverter;
 import uk.gov.justice.services.common.converter.StringToJsonObjectConverter;
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.InvalidStreamIdException;
-import uk.gov.justice.services.messaging.DefaultEnvelope;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.DefaultJsonEnvelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 import uk.gov.justice.services.messaging.JsonObjectEnvelopeConverter;
 import uk.gov.justice.services.messaging.JsonObjectMetadata;
 import uk.gov.justice.services.messaging.Metadata;
@@ -15,7 +15,7 @@ import javax.json.JsonObject;
 import java.util.UUID;
 
 /**
- * Converter class to convert between {@link Envelope} and {@link EventLog}
+ * Converter class to convert between {@link JsonEnvelope} and {@link EventLog}
  */
 @ApplicationScoped
 public class EventLogConverter {
@@ -37,7 +37,7 @@ public class EventLogConverter {
      * @param version  the version of the event
      * @return the database entity created from the given envelope
      */
-    public EventLog createEventLog(final Envelope envelope, final UUID streamId, final Long version) {
+    public EventLog createEventLog(final JsonEnvelope envelope, final UUID streamId, final Long version) {
 
         if (streamId == null) {
             throw new InvalidStreamIdException("StreamId missing in envelope.");
@@ -55,13 +55,13 @@ public class EventLogConverter {
     }
 
     /**
-     * Creates an {@link Envelope} from {@link EventLog}
+     * Creates an {@link JsonEnvelope} from {@link EventLog}
      *
      * @param eventLog eventLog to be converted into an envelope.
      * @return an envelope created from eventLog.
      */
-    public Envelope createEnvelope(final EventLog eventLog) {
-        return DefaultEnvelope.envelopeFrom(getMetaData(eventLog), getPayload(eventLog));
+    public JsonEnvelope createEnvelope(final EventLog eventLog) {
+        return DefaultJsonEnvelope.envelopeFrom(getMetaData(eventLog), getPayload(eventLog));
     }
 
     /**
@@ -78,7 +78,7 @@ public class EventLogConverter {
         return stringToJsonObjectConverter.convert(eventLog.getPayload());
     }
 
-    private String extractPayloadAsString(final Envelope envelope) {
+    private String extractPayloadAsString(final JsonEnvelope envelope) {
         return jsonObjectToStringConverter.convert(jsonObjectEnvelopeConverter.extractPayloadFromEnvelope(
                 jsonObjectEnvelopeConverter.fromEnvelope(envelope)));
     }

event-sourcing/event-repository/event-repository-jdbc/src/test/java/uk/gov/justice/services/eventsourcing/repository/jdbc/JdbcEventRepositoryTest.java
@@ -12,7 +12,7 @@ import uk.gov.justice.services.eventsourcing.repository.jdbc.eventlog.JdbcEventL
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.EventLogRepositoryException;
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.InvalidSequenceIdException;
 import uk.gov.justice.services.eventsourcing.repository.jdbc.exception.InvalidStreamIdException;
-import uk.gov.justice.services.messaging.Envelope;
+import uk.gov.justice.services.messaging.JsonEnvelope;
 import uk.gov.justice.services.messaging.Metadata;
 
 import java.util.Arrays;
@@ -44,7 +44,7 @@ public class JdbcEventRepositoryTest {
     private EventLogConverter eventLogConverter;
 
     @Mock
-    private Envelope envelope;
+    private JsonEnvelope envelope;
 
     @Mock
     private Metadata metadata;
@@ -60,7 +60,7 @@ public class JdbcEventRepositoryTest {
         when(jdbcEventLogRepository.findByStreamIdOrderBySequenceIdAsc(STREAM_ID)).thenReturn(Arrays.asList(eventLog).stream());
         when(eventLogConverter.createEnvelope(eventLog)).thenReturn(envelope);
 
-        Stream<Envelope> streamOfEnvelopes = jdbcEventRepository.getByStreamId(STREAM_ID);
+        Stream<JsonEnvelope> streamOfEnvelopes = jdbcEventRepository.getByStreamId(STREAM_ID);
 
         assertThat(streamOfEnvelopes, not(nullValue()));
         assertThat(streamOfEnvelopes.findFirst().get(), equalTo(envelope));
@@ -71,7 +71,7 @@ public class JdbcEventRepositoryTest {
         when(jdbcEventLogRepository.findByStreamIdFromSequenceIdOrderBySequenceIdAsc(STREAM_ID, VERSION_1)).thenReturn(Arrays.asList(eventLog).stream());
         when(eventLogConverter.createEnvelope(eventLog)).thenReturn(envelope);
 
-        Stream<Envelope> streamOfEnvelopes = jdbcEventRepository.getByStreamIdAndSequenceId(STREAM_ID, VERSION_1);
+        Stream<JsonEnvelope> streamOfEnvelopes = jdbcEventRepository.getByStreamIdAndSequenceId(STREAM_ID, VERSION_1);
 
         assertThat(streamOfEnvelopes, not(nullValue()));
         assertThat(streamOfEnvelopes.findFirst().get(), equalTo(envelope));
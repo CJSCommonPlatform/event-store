afc44c078cbf5525d276dbce3194a955bf00e7d5
Martin Spasov <martin.y.spasov@gmail.com>
2018-03-07 16:33:31 +0000

    Event source JDBC refactor.

--- event-sourcing/event-source/src/test/java/uk/gov/justice/services/eventsourcing/source/core/DefaultEventSourceTest.java
+++ event-sourcing/event-source/src/test/java/uk/gov/justice/services/eventsourcing/source/core/DefaultEventSourceTest.java
@@ -1,15 +1,23 @@
 package uk.gov.justice.services.eventsourcing.source.core;
 
+import static java.time.ZonedDateTime.now;
 import static java.util.UUID.randomUUID;
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import uk.gov.justice.services.eventsourcing.repository.jdbc.DefaultEventStreamMetadata;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.EventRepository;
+import uk.gov.justice.services.eventsourcing.repository.jdbc.EventStreamMetadata;
 import uk.gov.justice.services.eventsourcing.source.core.exception.EventStreamException;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,6 +35,9 @@ public class DefaultEventSourceTest {
 
     @Mock
     private EventStreamManager eventStreamManager;
+
+    @Mock
+    private EventRepository eventRepository;
 
     @Test
     public void shouldReturnEventStream() {
@@ -49,5 +60,36 @@ public class DefaultEventSourceTest {
         eventSource.clearStream(STREAM_ID);
 
         verify(eventStreamManager).clear(STREAM_ID);
+    }
+
+    @Test
+    public void shouldGetEventStreamsByPosition() {
+        final UUID streamId = randomUUID();
+        final long position = 1L;
+
+        final Stream<EventStreamMetadata> eventStreamObjectStream = Stream.of(new DefaultEventStreamMetadata(streamId, position, true, now()));
+        when(eventRepository.getEventStreamsFromPosition(position)).thenReturn(eventStreamObjectStream);
+
+        final Stream<uk.gov.justice.services.eventsourcing.source.core.EventStream> eventStreams = eventSource.getStreamsFrom(position);
+        List<uk.gov.justice.services.eventsourcing.source.core.EventStream> eventStreamList = eventStreams.collect(toList());
+
+        assertThat(eventStreamList.size(), is(1));
+        assertThat(eventStreamList.get(0), instanceOf(uk.gov.justice.services.eventsourcing.source.core.EventStream.class));
+        assertThat(eventStreamList.get(0), instanceOf(EnvelopeEventStream.class));
+        assertThat(eventStreamList.get(0).getId(), is(streamId));
+        assertThat(eventStreamList.get(0).getPosition(), is(position));
+    }
+
+
+    @Test
+    public void shouldReturnEmptyStream() {
+        final long position = 9L;
+
+        when(eventRepository.getEventStreamsFromPosition(position)).thenReturn(Stream.empty());
+
+        final Stream<uk.gov.justice.services.eventsourcing.source.core.EventStream> eventStreams = eventSource.getStreamsFrom(position);
+        List<uk.gov.justice.services.eventsourcing.source.core.EventStream> eventStreamList = eventStreams.collect(toList());
+
+        assertThat(eventStreamList.size(), is(0));
     }
 }

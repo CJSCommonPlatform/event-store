c97219de14ecb98e1e924ade229b8326c8683b5b
Shaun Francis <shaun@mapingo.co.uk>
2018-11-02 13:49:54 +0000

    Remove requirement for subscription decscriptor yaml

--- event-sourcing/event-subscription-registry/src/main/java/uk/gov/justice/subscription/registry/SubscriptionsDescriptorsRegistryProducer.java
+++ event-sourcing/event-subscription-registry/src/main/java/uk/gov/justice/subscription/registry/SubscriptionsDescriptorsRegistryProducer.java
@@ -1,6 +1,7 @@
 package uk.gov.justice.subscription.registry;
 
 import static java.lang.String.format;
+import static java.util.Collections.emptySet;
 import static java.util.stream.Collectors.toSet;
 
 import uk.gov.justice.subscription.SubscriptionsDescriptorParser;
@@ -20,8 +21,8 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 
 /**
- * Producer for the {@link SubscriptionsDescriptorsRegistry} creates a single instance and returns the same
- * instance.
+ * Producer for the {@link SubscriptionsDescriptorsRegistry} creates a single instance and returns
+ * the same instance.
  */
 @ApplicationScoped
 public class SubscriptionsDescriptorsRegistryProducer {
@@ -43,8 +44,8 @@ public class SubscriptionsDescriptorsRegistryProducer {
                             logger.info(format("Subscription name in registry : %s", subscription.getName())));
 
     /**
-     * Either creates the single instance of the {@link SubscriptionsDescriptorsRegistry} and returns it, or
-     * returns the previously created instance.
+     * Either creates the single instance of the {@link SubscriptionsDescriptorsRegistry} and
+     * returns it, or returns the previously created instance.
      *
      * @return the instance of the {@link SubscriptionsDescriptorsRegistry}
      */
@@ -54,15 +55,17 @@ public class SubscriptionsDescriptorsRegistryProducer {
             try {
                 final List<URL> subscriptionsDescriptorsPaths = yamlFileFinder.getSubscriptionsDescriptorsPaths();
 
-                if (subscriptionsDescriptorsPaths.isEmpty()){
-                    throw new RegistryException("No event-sources.yaml files found!");
+                if (subscriptionsDescriptorsPaths.isEmpty()) {
+                    logger.info("Failed to find yaml/subscriptions-descriptor.yaml resources on the classpath");
+                    subscriptionsDescriptorsRegistry = new SubscriptionsDescriptorsRegistry(emptySet());
+                } else {
+
+                    final Set<SubscriptionsDescriptor> subscriptionsDescriptors = subscriptionsDescriptorParser
+                            .getSubscriptionDescriptorsFrom(subscriptionsDescriptorsPaths)
+                            .peek(logRegisteredSubscriptionNames).collect(toSet());
+
+                    subscriptionsDescriptorsRegistry = new SubscriptionsDescriptorsRegistry(subscriptionsDescriptors);
                 }
-
-                final Set<SubscriptionsDescriptor> subscriptionsDescriptors = subscriptionsDescriptorParser
-                        .getSubscriptionDescriptorsFrom(subscriptionsDescriptorsPaths)
-                        .peek(logRegisteredSubscriptionNames).collect(toSet());
-
-                subscriptionsDescriptorsRegistry = new SubscriptionsDescriptorsRegistry(subscriptionsDescriptors);
             } catch (final IOException e) {
                 throw new RegistryException("Failed to find yaml/subscriptions-descriptor.yaml resources on the classpath", e);
             }

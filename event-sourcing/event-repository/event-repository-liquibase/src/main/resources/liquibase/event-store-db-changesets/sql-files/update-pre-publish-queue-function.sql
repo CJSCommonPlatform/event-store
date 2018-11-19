7fcfbd0fc304bf4d3b9016c6182226cac1a9f327
amckenzie <allan.mckenzie@agilesphere.co.uk>
2018-11-19 11:41:23 +0000

    Rename publish_queue table to pre_publish_queue

--- event-sourcing/event-repository/event-repository-liquibase/src/main/resources/liquibase/event-store-db-changesets/sql-files/update-pre-publish-queue-function.sql
+++ event-sourcing/event-repository/event-repository-liquibase/src/main/resources/liquibase/event-store-db-changesets/sql-files/update-pre-publish-queue-function.sql
@@ -1,7 +1,4 @@
---
--- This function has been replaced by update-pre-publish-queue-function.sql
--- 
-CREATE OR REPLACE FUNCTION update_publish_queue()
+CREATE OR REPLACE FUNCTION update_pre_publish_queue()
  RETURNS trigger
  LANGUAGE plpgsql
 AS $function$
@@ -9,7 +6,7 @@ AS $function$
 BEGIN
 
 IF (TG_OP = 'INSERT') THEN
-  INSERT into publish_queue(
+  INSERT into pre_publish_queue(
     event_log_id,
     date_queued)
   values(NEW.id, timezone('UTC', now()));

31d53d2308c002eb2393b88d127b66b931ca70ea
Shaun Francis <shaun@mapingo.co.uk>
2019-06-18 11:57:36 +0100

    Add CommandHandlerQueueChecker to check handler queue when shuttering

---
+++ event-store-management/src/main/java/uk/gov/justice/services/eventstore/management/shuttering/process/CommandHandlerQueueChecker.java
@@ -0,0 +1,22 @@
+package uk.gov.justice.services.eventstore.management.shuttering.process;
+
+import uk.gov.justice.services.common.polling.MultiIteratingPoller;
+import uk.gov.justice.services.common.polling.MultiIteratingPollerFactory;
+
+import javax.inject.Inject;
+
+public class CommandHandlerQueueChecker {
+
+    @Inject
+    private CommandHandlerQueueSupplierFactory commandHandlerQueueSupplierFactory;
+
+    @Inject
+    private MultiIteratingPollerFactory multiIteratingPollerFactory;
+
+    public boolean pollUntilEmptyHandlerQueue() {
+
+        final MultiIteratingPoller multiIteratingPoller = multiIteratingPollerFactory.create(5, 1000L, 3, 500L);
+
+        return multiIteratingPoller.pollUntilTrue(commandHandlerQueueSupplierFactory.isCommandHandlerQueueEmpty());
+    }
+}
---
+++ event-store-management/src/test/java/uk/gov/justice/services/eventstore/management/shuttering/process/CommandHandlerQueueSupplierFactoryTest.java
@@ -0,0 +1,53 @@
+package uk.gov.justice.services.eventstore.management.shuttering.process;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+
+import uk.gov.justice.services.messaging.jms.JmsCommandHandlerDestinationNameProvider;
+import uk.gov.justice.services.messaging.jms.JmsQueueBrowser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CommandHandlerQueueSupplierFactoryTest {
+
+    @Mock
+    private JmsQueueBrowser jmsQueueBrowser;
+
+    @Mock
+    private JmsCommandHandlerDestinationNameProvider jmsCommandHandlerDestinationNameProvider;
+
+    @InjectMocks
+    private CommandHandlerQueueSupplierFactory commandHandlerQueueSupplierFactory;
+
+    @Test
+    public void shouldReturnTrueWhenCheckingCommandHandlerQueueIsEmptyIfQueueSizeIsZero() {
+
+        final String destination = "destination.name";
+
+        when(jmsCommandHandlerDestinationNameProvider.destinationName()).thenReturn(destination);
+        when(jmsQueueBrowser.sizeOf(destination)).thenReturn(0);
+
+        final boolean result = commandHandlerQueueSupplierFactory.isCommandHandlerQueueEmpty().getAsBoolean();
+
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenCheckingCommandHandlerQueueIsEmptyIfQueueSizeIsGreaterThanZero() {
+
+        final String destination = "destination.name";
+
+        when(jmsCommandHandlerDestinationNameProvider.destinationName()).thenReturn(destination);
+        when(jmsQueueBrowser.sizeOf(destination)).thenReturn(1);
+
+        final boolean result = commandHandlerQueueSupplierFactory.isCommandHandlerQueueEmpty().getAsBoolean();
+
+        assertThat(result, is(false));
+    }
+}

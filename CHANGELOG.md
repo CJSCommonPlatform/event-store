528468ff5298b3b1900148d7e2113e49c7182a95
amckenzie <allan.mckenzie@agilesphere.co.uk>
2019-08-28 15:48:54 +0100

    Update framework to 6.0.10

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ on [Keep a CHANGELOG](http://keepachangelog.com/). This project adheres to
 
 ## [Unreleased]
 
+## [2.0.10] - 2019-08-28
+### Changed
+- Update framework to 6.0.10
+
 ## [2.0.9] - 2019-08-21
 ### Changed
 - Update framework to 6.0.9
--- interceptors/subscription-event-interceptors/src/test/java/uk/gov/justice/services/event/source/subscriptions/interceptors/it/SubscriptionEventInterceptorIT.java
+++ interceptors/subscription-event-interceptors/src/test/java/uk/gov/justice/services/event/source/subscriptions/interceptors/it/SubscriptionEventInterceptorIT.java
@@ -52,6 +52,7 @@ import uk.gov.justice.services.core.json.FileBasedJsonSchemaValidator;
 import uk.gov.justice.services.core.json.JsonSchemaLoader;
 import uk.gov.justice.services.core.json.PayloadExtractor;
 import uk.gov.justice.services.core.json.SchemaCatalogAwareJsonSchemaValidator;
+import uk.gov.justice.services.core.json.SchemaValidationErrorMessageGenerator;
 import uk.gov.justice.services.core.mapping.ActionNameToMediaTypesMappingObserver;
 import uk.gov.justice.services.core.mapping.DefaultMediaTypesMappingCache;
 import uk.gov.justice.services.core.mapping.DefaultNameToMediaTypeConverter;
@@ -211,7 +212,9 @@ public class SubscriptionEventInterceptorIT {
             ComponentNameExtractor.class,
 
             JndiAppNameProvider.class,
-            InitialContextProducer.class
+            InitialContextProducer.class,
+
+            SchemaValidationErrorMessageGenerator.class
     })
 
     public WebApp war() {
